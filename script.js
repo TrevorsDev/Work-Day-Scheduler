99791ae27c697ae117d782afbddaac3e74ef9291
Trevor Johnson <trevor9j@protonmail.com>
2024-03-12 13:14:54 -0700

    corrected the time in each hour of the scheduler

---
+++ script.js
@@ -0,0 +1,21 @@
+var saveButton = $('.saveBtn');
+
+var save = function () {
+
+    var value = $(this).siblings('.description').val();
+    var hour = $(this).parent().attr('id');
+
+    localStorage.setItem(hour, value);
+};
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+saveButton.on('click', save);
